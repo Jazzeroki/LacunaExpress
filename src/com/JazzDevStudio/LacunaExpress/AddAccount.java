a440008ad781bbc7d8d4bce3cbc5131068e8ea8e
pgmacdesign <pgmacdesign@gmail.com>
2014-12-06 16:24:48 -0800

    Fixed the login bug. Declared variables as global for usage and defined them via the initialize method. Also added radiobutton and checkbox listeners.

--- src/com/JazzDevStudio/LacunaExpress/AddAccount.java
+++ src/com/JazzDevStudio/LacunaExpress/AddAccount.java
@@ -10,21 +10,35 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 
-
-public class AddAccount extends Activity implements serverFinishedListener {
+public class AddAccount extends Activity implements serverFinishedListener, OnClickListener, OnCheckedChangeListener, android.widget.CompoundButton.OnCheckedChangeListener {
 
     private AccountInfo account = new AccountInfo();
+    
+    //Global Variables
+    Button bLogin, bAddAccount;
+    RadioGroup rg;
+    CheckBox cbdfAccount;
+    EditText etusername, etpassword;
+    
+    String server;
+    boolean remember_me;
+    
     public void onResponseRecieved(String reply) {
         //This is the listener to the server event
         //if server request errors it returns error
@@ -43,54 +57,58 @@ public class AddAccount extends Activity implements serverFinishedListener {
 
             account.sessionID = r.result.session_id;
 
-            AccountMan.AccountMan a = new AccountMan.AccountMan();
-            a.AddAccount(account);
+            AccountMan.AccountMan acm = new AccountMan.AccountMan();
+            acm.AddAccount(account);
 
             Intent intent = new Intent(this, SelectAccount.class);
-            startActivity(intent);
-            
+            startActivity(intent);          
         }
-
-    }
-
-    public void OnLoginClick(View v){
-        EditText etusername = (EditText)findViewById(R.id.add_account_username);
-        account.userName = etusername.getText().toString();
-        EditText etpassword = (EditText)findViewById(R.id.add_account_password);
-        account.password = etpassword.getText().toString();
-        RadioGroup rg=(RadioGroup)findViewById(R.id.add_account_server_choices);
-        CheckBox cbdfAccount = (CheckBox)findViewById(R.id.add_account_default); //Changed this line
-        account.defaultAccount = cbdfAccount.isChecked();
-
-        String server ="";
-        int serverid = rg.getCheckedRadioButtonId();
-        switch (serverid) {
-            case 0: account.server = "https://us1.lacunaexpanse.com";
-                break;
-            case 1: account.server = "https://pt.lacunaexpanse.com";
-                break;
-        }
-        //if all required fields are filled in then the request will be sent to the server
-        if(!account.userName.isEmpty()&&!account.password.isEmpty()&&!account.server.isEmpty()){
-            Empire e = new Empire();
-            String request = e.Login(account.userName, account.password, 1);
-            ServerRequest sRequest = new ServerRequest(server, Empire.url, request);
-            AsyncServer s = new AsyncServer();
-            s.addListener(this);
-            s.execute(sRequest);
-            //AsyncServer clears all listeners after the requests have been recieved.
-        }
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_account);
+        
+        //Clearing up the onCreate by adding this method, initializes variables
+        Initialize();
     }
 
+    /*
+     * Helps un-clutter the onCreate. All of this code could go int
+     * the onCreate method, but it makes for easier viewing/ reading.
+     * Note, NO new variables are defined here else they would be 
+     * isolated to this class alone
+     */
+    //To Initialize Variables
+    private void Initialize() {
 
-    @Override
+    	//Server String set to nothing upon initialization
+    	server ="";
+    	
+    	//Default the remember me box to false
+    	remember_me = false;
+    	
+    	//Initialize Global Variables
+    	rg = (RadioGroup) findViewById(R.id.add_account_server_choices);
+    	etusername = (EditText) findViewById(R.id.add_account_username);
+    	etpassword = (EditText) findViewById(R.id.add_account_password);
+    	cbdfAccount = (CheckBox) findViewById(R.id.add_account_default);
+    	bLogin = (Button) findViewById(R.id.add_account_login);
+    	bAddAccount = (Button) findViewById(R.id.add_account_add_account);
+    	
+    	//Set the buttons to clickable
+    	bLogin.setOnClickListener(this);
+    	bAddAccount.setOnClickListener(this);
+    	
+    	//For the Radio button so it is waiting for the choice to be changes
+    	rg.setOnCheckedChangeListener(this);
+    	
+    	//For the Checkbox
+    	cbdfAccount.setOnCheckedChangeListener(this);
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         //
@@ -117,4 +135,92 @@ public class AddAccount extends Activity implements serverFinishedListener {
 
         return super.onOptionsItemSelected(item);
     }
+
+	//This handles the click methods of the buttons so when they are clicked, the actions are defined here
+	public void onClick(View v) {
+
+		switch (v.getId()){
+		
+		//When the login button is clicked
+		case R.id.add_account_login:
+			
+			try {
+				
+				account.userName = etusername.getText().toString();
+				account.password = etpassword.getText().toString();
+				
+		        //if all required fields are filled in then the request will be sent to the server
+		        if(!account.userName.isEmpty()&&!account.password.isEmpty()&&!account.server.isEmpty()){
+		            Empire e = new Empire();
+		            String request = e.Login(account.userName, account.password, 1);
+		            ServerRequest sRequest = new ServerRequest(server, Empire.url, request);
+		            AsyncServer s = new AsyncServer();
+		            s.addListener(this);
+		            s.execute(sRequest);
+		            Log.d("Login", "Login Success");
+		            //AsyncServer clears all listeners after the requests have been recieved.
+		        }
+				
+			} catch (Exception e){
+				e.printStackTrace();
+				Log.d("Error", "Login Error");
+			}
+			break;
+			
+		//When the add account button is clicked
+		case R.id.add_account_add_account:
+			try {
+			
+				account.userName = etusername.getText().toString();
+				account.password = etpassword.getText().toString();
+				
+				Log.d("Login", "Add Account Success");
+			} catch (Exception e){
+				e.printStackTrace();
+				Log.d("Error", "Add Account Error");
+			}
+			break;
+			
+		}
+		
+	}
+
+	//This handles the radio buttons
+	public void onCheckedChanged(RadioGroup rg, int checkedId) {
+
+		switch(checkedId){
+		
+		//US1, set the server choice to US1
+		case R.id.add_account_us1:
+			server = "https://us1.lacunaexpanse.com";
+			account.server = "https://us1.lacunaexpanse.com";
+			Log.d("Radio", "US1 Checked");
+			break;
+			
+		//PT, set the server choice to PT			
+		case R.id.add_account_pt:
+			server = "https://us1.lacunaexpanse.com";
+			account.server = "https://pt.lacunaexpanse.com";
+			Log.d("Radio", "PT Checked");
+			break;
+						
+		}
+		
+	}
+
+	//This covers the Checkbox. Same method as radiogroup, but overloaded for checkbox
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		//cbdfAccount <-- Checkbox
+		if (buttonView.isChecked()) { 
+			//checked
+			Log.d("Checkbox", "Default Button Checked");
+			remember_me = true;
+		} else {
+			//not checked
+			Log.d("Checkbox", "Default Button Unchecked");
+			remember_me = false;
+		} 
+
+	}
+	
 }
