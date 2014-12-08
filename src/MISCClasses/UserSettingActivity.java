d6f3a0c246a83d655ea43db79dd1b7eb87cdd4a0
pgmacdesign <pgmacdesign@gmail.com>
2014-12-08 00:31:23 -0800

    Adding settings menu

---
+++ src/MISCClasses/UserSettingActivity.java
@@ -0,0 +1,17 @@
+package MISCClasses;
+
+import com.JazzDevStudio.LacunaExpress.R;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class UserSettingActivity extends PreferenceActivity {
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        addPreferencesFromResource(R.xml.settings);
+ 
+    }
+}
