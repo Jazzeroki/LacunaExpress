252a9aeec9ee8ea6346f1b3c0987d0096c8f6438
pgmacdesign <pgmacdesign@gmail.com>
2014-12-29 12:43:43 -0800

    Adding new packages to manage the database stuff

---
+++ app/src/main/java/com/JazzDevStudio/LacunaExpress/Database/DatabaseContract.java
@@ -0,0 +1,10 @@
+package com.JazzDevStudio.LacunaExpress.Database;
+
+/**
+ * Created by PatrickSSD2 on 12/29/2014.
+ */
+public class DatabaseContract {
+
+	//
+	public static final String TABLE_NAME = "T";
+}
