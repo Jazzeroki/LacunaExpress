52e37f634cbb439b4fb6d85411b92ae356f709fe
Alma <falcont40@hotmail.com>
2015-01-27 21:44:41 -0700

    Stubs for captcha support
    Added stub code files for captcha support

---
+++ app/src/main/java/com/JazzDevStudio/LacunaExpress/Captcha/CaptchaResponse.java
@@ -0,0 +1,7 @@
+package com.JazzDevStudio.LacunaExpress.Captcha;
+
+/**
+ * Created by Alma on 1/27/2015.
+ */
+public class CaptchaResponse {
+}
---
+++ app/src/main/java/com/JazzDevStudio/LacunaExpress/Captcha/CheckCaptcha.java
@@ -0,0 +1,8 @@
+package com.JazzDevStudio.LacunaExpress.Captcha;
+
+/**
+ * Created by Alma on 1/27/2015.
+ */
+public class CheckCaptcha {
+    
+}
